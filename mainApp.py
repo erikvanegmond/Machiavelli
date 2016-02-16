8fff12635110fb517bbdfc65929522b820d0accb
Erik van Egmond <erikvane@gmail.com>
2016-02-17 00:06:59 +0100

    Added players to the game (Hardcoded), added update state button

--- mainApp.py
+++ mainApp.py
@@ -36,20 +36,29 @@ class GameTakeAction(tornado.web.RequestHandler):
 class GameUpdate(tornado.web.RequestHandler):
     """ Posting to this method will resolve an action in the game state """
 
+    def initialize(self, gsc):
+        self.gsc = gsc
+
     def post(self):
+        self.gsc.update_state()
         self.write("Update")  # TODO: Post update to game controller.
 
 
 class Application(tornado.web.Application):
     def __init__(self):
         self.gsc = game_state_controller.GameStateController(4, game_name="My Game")
+        self.gsc.add_player("Erik")
+        self.gsc.add_player("Justin")
+        self.gsc.add_player("Abigail")
+        self.gsc.add_player("Sabrina")
+
 
         handlers = [
             (r"/", MainHandler),
             (r"/game/state", GameState, dict(gsc=self.gsc)),
             (r"/reset", GameReset),  # Technical alpha single game version only.
             (r"/game/action", GameTakeAction),
-            (r"/game/update", GameUpdate)
+            (r"/game/update", GameUpdate, dict(gsc=self.gsc))
         ]
         settings = {
             "debug": True
